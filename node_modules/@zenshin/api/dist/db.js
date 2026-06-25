a8ce54d44f7b970f16c6a1d0dd39596d305bb33b
vishalblackbelt-cmd <vishalblackbelt@gmail.com>
2026-06-26 00:21:17 +0530

    chore: untrack node_modules

--- node_modules/@zenshin/api/dist/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const db_1 = require("@zenshin/db");
-Object.defineProperty(exports, "prisma", { enumerable: true, get: function () { return db_1.prisma; } });
